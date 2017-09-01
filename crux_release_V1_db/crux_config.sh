9ab24dc4bf27e12dca2fb84d92012dfb1c242f8d
limey-bean <eecurd@g.ucla.edu>
2017-09-01 15:50:07 -0700

    Update crux_config.sh

--- crux_release_V1_db/crux_config.sh
+++ crux_release_V1_db/crux_config.sh
@@ -38,6 +38,11 @@ TAXO="${DB}/TAXO" 											#see documentation for how to obtain this directory
 #NCBI key for accession number to taxonomy assignment
 A2T="${DB}/accession2taxonomy/nucl_gb.accession2taxid" 		#see documentation for how to obtain this file
 
+#BLAST db folder.  Should contain many files like the following: nt.00.nhr, nt.00.nin, nt.00.nnd, nt.00.nni, nt.00.nsq, nt.01.nhr, nt.01.nin, nt.01.nnd, nt.01.nni, nt.01.nsq, nt.01.nhr, nt.02.nin,......,......,nt.nal
+BLAST_DB="${DB}/NCBI_blast_nt/nt"							#see documentation for how to obtain this director
+
+
+
 
 
 
