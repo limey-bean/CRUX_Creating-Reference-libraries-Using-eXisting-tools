36b9841f824b48c7e8493e961d50455c8a48f187
Max Ogden <max@maxogden.com>
2018-03-05 14:57:07 +0800

    fix templates

--- crux_release_V1_db/scripts/crux_qsub_templates.sh
+++ crux_release_V1_db/scripts/crux_qsub_templates.sh
@@ -2,10 +2,10 @@
 # if your environment needs different arguments for your job scheduling system you can change these
 # example usage:
 # source crux_qsub_templates.sh
-# printf "$(CRUX_PART1_BLAST1_TEMPLATE)" > some_file
+# echo "$(CRUX_PART1_BLAST1_TEMPLATE)" > some_file
 
 CRUX_PART1_BLAST1_TEMPLATE() {
-read -d '' String <<"EOF"
+read -d '' String <<EOF
 #!/bin/bash
 #$ -l highp,h_rt=05:00:00,h_data=30G
 #$ -N blast1_${j}_${NAME}
@@ -18,11 +18,11 @@ read -d '' String <<"EOF"
 
 sh ${DB}/scripts/sub_blast1.sh -n ${NAME} -q ${nam1}_${array_var} -o ${ODIR} -j ${j} -l blast_ready_${array_var} -d ${DB} 
 EOF
-echo "${String}" 
+echo "$String" 
 }
 
 CRUX_PART1_BLAST2_TEMPLATE() {
-read -d '' String <<"EOF"
+read -d '' String <<EOF
 #!/bin/bash
 #$ -l highp,h_rt=05:00:00,h_data=30G
 #$ -N blast2_${j}_${NAME}
@@ -36,11 +36,5 @@ read -d '' String <<"EOF"
 
 sh ${DB}/scripts/sub_blast2.sh -n ${NAME} -q ${nam1}_${array_var} -o ${ODIR} -j ${j} -l blast_ready_${array_var} -d ${DB} 
 EOF
-echo "${String}"
-}
-
-
-
-DADA2_PAIRED_TEMPLATE() {
-  echo "#!/bin/bash\n#$ -q std.q\n#$ -cwd\n#$ -l mem_free=48G\n#$ -pe smp 1\n#$ -N paired_${j}_dada2\n#$ -o ${OUT}/Run_info/hoffman2/run_logs/${j}_paired_$JOB_ID.out\n#$ -e ${OUT}/Run_info/hoffman2/run_logs/${j}_paired_$JOB_ID.err\n\necho _BEGIN_ [run_dada2_bowtie2_paired.sh]: `date`\n\n${RUNNER} ${DB}/scripts/run_dada2.sh  -o ${OUT} -d ${DB} -m ${j} -t paired\n\necho _END_ [run_dada2_paired.sh]"
+echo "$String" 
 }
